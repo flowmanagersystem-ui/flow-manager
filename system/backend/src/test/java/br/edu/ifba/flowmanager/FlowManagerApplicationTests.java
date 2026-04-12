696d794d18720018e60fe18e992340d0c4206410
Johnison pc-ubuntu <johnisonbsi@outlook.com>
2026-04-12 02:35:12 -0300

    feat: Implementar estrutura básica do backend com Maven e Spring Boot

---
+++ system/backend/src/test/java/br/edu/ifba/flowmanager/FlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.flowmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
